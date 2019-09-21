9eb5367565aec94a0b8e16ee632bca0911657b58
Vytas <gavelisvytas@gmail.com>
2019-09-21 13:07:02 +0300

    Add files via upload
    +Statistics.php clients can now see most busytimes of selected specialist

---
+++ public/statistics.php
@@ -0,0 +1,120 @@
+<?php
+session_start();
+
+include_once('../resources/dbconnection.php');
+include_once('../src/SpecialistService.php');
+
+if(isset($_GET['id'])){
+    $_SESSION['id'] = $_GET['id'];
+    $times = array();
+
+    for($i = 0; $i < 24; $i++){
+        if($i < 10){
+            $dateL = '0' . $i . ':00:00';
+            $dateH = '0' . $i . ':59:59';
+            $hour = '0' . $i;
+        }
+        else{
+            $dateL = $i . ':00:00';
+            $dateH = $i . ':59:59';
+            $hour = $i;
+        }
+        $stmt = $pdo->prepare("SELECT * FROM clients WHERE TIME(date) between :dateL AND :dateH AND specialists_id = :id");
+        $stmt->execute(array(
+            ':dateL' => $dateL,
+            ':dateH' => $dateH,
+            ':id' => $_GET['id']
+        ));
+        $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        
+        $dataArr = array( 
+            "time" => $hour,
+            "count" => sizeof($data)
+        );
+        // Append data of certain hour to times array
+        array_push($times, $dataArr);
+    }
+}
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <link rel="stylesheet" type="text/css" href="css/board.css">
+</head>
+
+<script>
+
+// Load google charts
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+
+// Draw the chart and set the chart values
+function drawChart() {
+  var data = google.visualization.arrayToDataTable([
+  ['Time', 'Count'],
+  <?php
+    foreach($times as $time){
+        echo "['" . $time['time'] . "', " . $time['count'] . "], ";
+    }
+  ?>
+]);
+
+var options = {
+    'width':600,
+    'height':450,
+    'backgroundColor': '#1ab188'
+};
+
+var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+chart.draw(data, options);
+}
+
+</script>
+<body>
+<?php include_once("navbar.html"); ?>
+
+    <div class="row">
+        <div class="column side"></div>
+
+        <div class="column middle">
+            <div class="form">
+                <form>
+                    <select name="id">
+                        <?php
+                            // Fill the select options with specialists.
+                            $specialistService = new SpecialistService($pdo);
+                            $specialists = $specialistService->getAllSpecialists();
+                            foreach ($specialists as $specialist) {
+                                if(isset($_SESSION['id']) && $specialist->getId() == $_SESSION['id']){
+                                    echo "<option selected='selected' value='" . $specialist->getId() . "'>" . $specialist->getName() . "</option>";
+                                    unset($_SESSION['id']);
+                                }else{
+                                    echo "<option value='" . $specialist->getId() . "'>" . $specialist->getName() . "</option>";
+                                }
+                                echo $specialist->getId() . " " . $_SESSION['id'];
+                            //
+                            }
+                        ?>
+                    </select>
+                    <button type="submit">Rodyti</button>
+                </form>
+            </div>
+            <div>
+                <?php 
+                    if(isset($_GET['id'])){
+                        echo "Labiausiai užimti specialisto laikai:";
+                    }
+                    else{
+                        echo "Pasirinkite specialistą ir spauskite rodyti, kad matyti jo laikus.";
+                    }
+                ?>
+            </div>
+            <div id="piechart" align='center'></div>
+        </div>
+
+        <div class="column side"></div>
+    </div>
+</body>
+</html>
