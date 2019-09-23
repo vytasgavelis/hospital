9d019e7344703da96b47226d5f4ae16db2622a13
Vytas <gavelisvytas@gmail.com>
2019-09-23 20:40:55 +0300

    Add files via upload
    Last commit

--- resources/config.php
+++ resources/config.php
@@ -2,8 +2,8 @@
 $config = array(
     'db' => array(
         'server' => 'eu-cdbr-west-02.cleardb.net',
-        'username' => 'b2b87a1b64233a',
-        'password' => 'c89b578d',
-        'dbname' => 'heroku_6472ed31cc83478'
+        'username' => 'b60e10aa033b55',
+        'password' => '64299725',
+        'dbname' => 'heroku_dbcbbbd3aff02e8'
     )
 );
